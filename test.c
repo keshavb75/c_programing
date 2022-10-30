688bff97b23b3e9a6e93ce1c713325263a26383a
keshavb75 <keshavb75@gmail.com>
2022-10-30 13:57:24 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 void main()
 {
-  printf("Hello World");
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+    if (num % 2==0)
+	{
+		printf("number is even");
+	}
+	else
+	{
+		printf("number is odd");
+	}
+
 }
