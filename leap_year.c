0398dd4f16983322c933ba83085bb2161b9c238b
keshavb75 <keshavb75@gmail.com>
2022-10-30 14:31:15 +0530

    Update leap_year.c

--- leap_year.c
+++ leap_year.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+void main()
+{
+	int yr;
+	printf("enter the year");
+	scanf("%d",&yr);
+	if(yr%4==0)
+	{
+		printf("year is leap");
+	}
+	else
+	{
+		printf("year is not leap");
+	}
+}
